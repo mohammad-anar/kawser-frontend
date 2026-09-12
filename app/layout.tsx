7affea33e8ce6b16ff9c64c833a1d824344d2988
mohammad-anar <ai.anarul4232@gmail.com>
2026-09-12 22:31:42 +0600

    feat: add root layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -330,7 +330,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '2477817319364552');
+              fbq('init', '1352224156721408');
               fbq('track', 'PageView');
             `,
           }}
@@ -341,7 +341,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=2477817319364552&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1352224156721408&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
